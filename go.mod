e66e787004ccc4c221de2ad68bea523de4a6dd44
Nicolas Pereyra <npereyra215@gmail.com>
2023-02-23 14:40:02 -0300

    ADD: /products/:id GET; PUT. Basic Merging on PUT

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/pluralsight/inventoryservice
 
 go 1.19
+
+require github.com/imdario/mergo v0.3.13 // indirect
--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"encoding/json"
+	"github.com/imdario/mergo"
+	"io/ioutil"
 	"log"
 	"net/http"
-  "io/ioutil"
+	"strconv"
+	"strings"
 )
 
 type Product struct {
@@ -47,6 +50,15 @@ func getNextID() int {
 	return highestID + 1
 }
 
+func findProductById(productID int) (*Product, int) {
+	for i, product := range productList {
+		if product.ProductID == productID {
+			return &product, i
+		}
+	}
+	return nil, 0
+}
+
 func productsHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
@@ -83,7 +95,63 @@ func productsHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func productHandler(w http.ResponseWriter, r *http.Request) {
+	urlPathSegments := strings.Split(r.URL.Path, "products/")
+	productID, err := strconv.Atoi(urlPathSegments[len(urlPathSegments)-1])
+	if err != nil {
+		log.Fatal(err)
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	product, listItemIndex := findProductById(productID)
+	if product == nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	switch r.Method {
+	case http.MethodGet:
+		productJSON, err := json.Marshal(product)
+		if err != nil {
+			log.Fatal(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(productJSON)
+	case http.MethodPut:
+		var updateProduct Product
+		bodyBytes, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Fatal(err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		err = json.Unmarshal(bodyBytes, &updateProduct)
+		if err != nil {
+			log.Fatal(err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if updateProduct.ProductID != productID {
+			log.Fatal(err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		err = mergo.Merge(&productList[listItemIndex], updateProduct, mergo.WithOverride)
+		if err != nil {
+			log.Fatal(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		return
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
 func main() {
 	http.HandleFunc("/products", productsHandler)
+	http.HandleFunc("/products/", productHandler)
 	http.ListenAndServe(":5000", nil)
 }
